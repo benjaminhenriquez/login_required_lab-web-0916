65d528df64b0ae837d7aa68e987ce14f54572b15
github username <github email address>
2016-10-25 01:47:28 -0400

    Done.

---
+++ app/controllers/secrets_controller.rb
@@ -0,0 +1,10 @@
+
+class SecretsController < ApplicationController
+  #
+  before_action :required_log_in
+  def show
+    unless session[:name]
+      redirect_to '/login'
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+
+    get '/login' => 'sessions#new'
+    post '/login' => 'sessions#create'
+    get 'logged' => 'sessions#show'
+    post '/logout' => 'sessions#destroy'
+    get '/secret' => 'secrets#show'
+    get '/login' => 'secrets#show'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
